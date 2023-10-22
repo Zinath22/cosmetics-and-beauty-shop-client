89150920282beed0b200825cf3f16b2bdc5413c5
Zinath22 <Zinath22@users.noreply.github.com>
2023-10-23 00:37:48 +0600

    login page correction

--- src/Pages/Product/ProductCard.jsx
+++ src/Pages/Product/ProductCard.jsx
@@ -1,55 +1,99 @@
-import { Link } from "react-router-dom";
-import PropTypes from 'prop-types'; 
-import { BsFillStarFill } from 'react-icons/Bs';
+// import { Link } from "react-router-dom";
+// import PropTypes from 'prop-types'; 
+// import { BsFillStarFill } from 'react-icons/Bs';
 
 
 
-const ProductCard = ({product}) => {
+// const ProductCard = ({product}) => {
 
     
-    const {_id, name, brand_name, type, price, description, rating, photo} = product;
+//     const {_id, name, brand_name, type, price, description, rating, photo} = product;
 
-    return (
+//     return (
         
        
         
-           <div className=" flex md:flex-row flex-col w-[300px] rounded-lg bg-base-100 shadow-xl">
-            <img className="w-[40%]" src={photo} alt="" />
- {/* <figure><img className="md:w-[250px] w-full h-screen md:h-[250px] " src={photo} alt="Movie"/></figure> */}
+//            <div className=" flex md:flex-row flex-col w-[300px] rounded-lg bg-base-100 shadow-xl">
+//             <img className="w-[40%]" src={photo} alt="" />
 
-  <div className="justify-between  flex "> 
- <div className="p-10"> 
- <h2 className="card-title">Name:{name}</h2>
-   <p>Type:{type}</p>
-   <p>Price: ${price}</p>
-   <p className="flex items-center gap-2"><span className="text-yellow-400"><BsFillStarFill></BsFillStarFill></span> {rating}</p>
- </div>
-     </div>
 
-   <div className="btn-group p-10 btn-group-vertical space-y-5 ">
- {/* <button className="btn ">View</button> */}
- <Link to={`/update/${_id}`}>
- <button className="btn bg-purple-400">Update</button>
- </Link>
+//   <div className="justify-between  flex "> 
+//  <div className="p-10"> 
+//  <h2 className="card-title">Name:{name}</h2>
+//    <p>Type:{type}</p>
+//    <p>Price: ${price}</p>
+//    <p className="flex items-center gap-2"><span className="text-yellow-400"><BsFillStarFill></BsFillStarFill></span> {rating}</p>
+//  </div>
+//      </div>
 
- <Link to={`/productDetail/${_id}`}>
- <button 
+//    <div className="btn-group p-10 btn-group-vertical space-y-5 ">
+//  {/* <button className="btn ">View</button> */}
+//  <Link to={`/update/${_id}`}>
+//  <button className="btn bg-purple-400">Update</button>
+//  </Link>
 
-className="btn bg-purple-400">Details</button>
- </Link>
+//  <Link to={`/productDetail/${_id}`}>
+//  <button 
 
-</div>
-</div>
+// className="btn bg-purple-400">Details</button>
+//  </Link>
+
+// </div>
+// </div>
 
     
           
      
            
+//     );
+// };
+
+// ProductCard.propTypes={
+//   product:PropTypes.object
+// }
+
+// export default ProductCard;
+
+
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+import { BsFillStarFill } from 'react-icons/Bs';
+
+const ProductCard = ({ product }) => {
+    const { _id, name, brand_name, type, price, description, rating, photo } = product;
+
+    return (
+      <div className="bg-base-100 shadow-xl rounded-lg">
+         <div className="flex flex-col md:flex-row ">
+
+<img className="w-full h-[200px] md:w-[40%]" src={photo} alt="" />
+
+<div className="p-4 flex-grow">
+    <h2 className="card-title">Name: {name}</h2>
+    <p>Type: {type}</p>
+    <p>Price: ${price}</p>
+    <p className="flex items-center gap-2">
+        <span className="text-yellow-400"><BsFillStarFill /></span> {rating}
+    </p>
+</div>
+
+
+</div>
+<div className="btn-group gap-5 mb-5 mt-5  p-5 btn-group-horizontal">
+    <Link to={`/update/${_id}`}>
+        <button className="btn bg-purple-400">Update</button>
+    </Link>
+    <Link to={`/productDetail/${_id}`}>
+        <button className="btn bg-purple-400">Details</button>
+    </Link>
+</div>
+</div>
+    
     );
 };
 
-ProductCard.propTypes={
-  product:PropTypes.object
-}
+ProductCard.propTypes = {
+    product: PropTypes.object,
+};
 
 export default ProductCard;
